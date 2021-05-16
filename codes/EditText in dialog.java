e3731c13331cfaacdb0a53c924ea89006f2bd273
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2021-05-16 21:06:52 +0530

    Create EditText in dialog.java

---
+++ codes/EditText in dialog.java
@@ -0,0 +1,9 @@
+final EditText
+myedittext = new EditText
+(Activitynamehere.this);
+dialog.setView(myedittext);
+/*BUTTON ONCLICK*/
+mytext = 
+myedittext.getText().to
+String();
+/*TUTORIAL: https://www.sketchwarehelp.ga/2020/08/Edittext-dialog.html */

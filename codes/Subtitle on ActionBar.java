33bab870ba3e2c215de5ee8e4fe3dfde99aa987a
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2021-05-16 21:07:52 +0530

    Update Subtitle on ActionBar.java

--- codes/Subtitle on ActionBar.java
+++ codes/Subtitle on ActionBar.java
@@ -1,3 +1,4 @@
 getSupportActionBar().setSubtitle("BYE");
 //ALTERNATIVELY USE THIS INSTEAD:
 getActionBar().setSubtitle("BYE");
+/*TUTORIAL: https://www.sketchwarehelp.ga/2021/04/how-to-set-subtitle-for-video-in.html */

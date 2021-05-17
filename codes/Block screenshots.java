c24f289f34fcf72ebf181d031f8507956f896f36
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2021-05-17 10:13:19 +0530

    Update Block screenshots.java

--- codes/Block screenshots.java
+++ codes/Block screenshots.java
@@ -1,1 +1,2 @@
 getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE;
+/*TUTORIAL: https://www.sketchwarehelp.ga/2020/08/how-to-block-screentshort-in-sketchware.html */
